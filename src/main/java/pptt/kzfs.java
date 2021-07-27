4e2e5c9690aa3be5d0d207149d57cba40c43b10e
abab <abab@qq.com>
2021-07-27 15:01:06 +0800

    hot-fix first commit

--- src/main/java/pptt/kzfs.java
+++ src/main/java/pptt/kzfs.java
@@ -3,5 +3,7 @@ package pptt;
 public class kzfs {
     public static void main(String[] args) {
         System.out.println("master");
+        System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
